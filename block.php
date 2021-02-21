0b3fd5ef630e4fd4e8dfdaf39202d7042fb22742
marin <marinb997@gmail>
2021-02-21 14:53:22 +0100

    Prikaz transakcija

--- block.php
+++ block.php
@@ -1,11 +1,40 @@
 <?php
 include 'template.php';
 if (isset($_POST['block'])) {
-    $block=json_decode($bitcoind->getBlock($_POST['block']));
+
+    try {
+        $testHash=$bitcoind->getBlock($_POST['block']);
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
+    if (isset($testHash)) {
+        $block=json_decode($bitcoind->getBlock($_POST['block']));
+    }
+    /*elseif (is_numeric($_POST['block'])) {
+        try {
+            $testHeight=$bitcoind->getblockhash(intval($_POST['block']));
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+        if (isset($testHeight)) {
+            $body=$testHeight->getBody();
+            $block_hash_arr=json_decode($body, true);
+            $block=json_decode($bitcoind->getblock($block_hash_arr['result']));
+    }
+        else {
+            echo '<h4>Wrong input!<h4>';
+            exit();
+        }
+    }*/
+    else {
+        echo '<h4>Wrong input!<h4>';
+        exit();
+    }
 }
 else {
     $block=json_decode($bitcoind->getBlock($_GET['hash']));
 }
+$block_info=json_decode($bitcoind->getblockstats($block->hash));
 
 echo '<br><br><br><br>';
 echo '<form action="block_transactions.php">';
@@ -23,7 +52,7 @@ echo '<table class="table table-striped" style="width:60%;margin-left:auto;margi
     echo '<tr><td>Nonce</td><td>'.$block->nonce.'</td></tr>';
     echo '<tr><td>Bits</td><td>'.$block->bits.'</td></tr>';
     echo '<tr><td>Difficulty</td><td>'.$block->difficulty.'</td></tr>';
-    echo '<tr><td>Time</td><td>'.$block->time.'</td></tr>';
+    echo '<tr><td>Time</td><td>'.gmdate("Y/m/d H:i:s",$block->time).'</td></tr>';
     echo '<tr><td>Size</td><td>'.$block->size.' KB</td></tr>';
     echo '<tr><td>Weight</td><td>'.$block->weight.' kWU</td></tr>';
     echo '<tr><td>Number of transactions</td><td>'.sizeof($block->tx).'</td></tr>';
@@ -35,5 +64,8 @@ echo '<table class="table table-striped" style="width:60%;margin-left:auto;margi
     else {
         echo '<tr><td>Next block hash</td><td></td></tr>';
     }
+    echo '<tr><td>Average fee</td><td>'.$block_info->avgfee.' satoshi</td></tr>';
+    echo '<tr><td>Total inputs</td><td>'.$block_info->ins.'</td></tr>';
+    echo '<tr><td>Total outputs</td><td>'.$block_info->outs.'</td></tr>';
 echo '</table>';
 ?>
